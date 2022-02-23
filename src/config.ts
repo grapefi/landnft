a1859e783e16cdea830999f327e8be1070d76dbb
grapefi <96745303+grapefi@users.noreply.github.com>
2022-02-24 03:09:26 +1100

    fix

--- src/config.ts
+++ src/config.ts
@@ -4,32 +4,7 @@ import { Configuration } from './tomb-finance/config';
 import { BankInfo } from './tomb-finance';
 
 const configurations: { [env: string]: Configuration } = {
-  development: {
-    chainId: 97,
-    networkName: 'Bsc Test Net',
-    ftmscanUrl: 'https://testnet.bscscan.com/',
-    defaultProvider: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
-    deployments: require('./tomb-finance/deployments/deployments.testing.json'),
-    externalTokens: {
-      WFTM: ['0xf1277d1ed8ad466beddf92ef448a132661956621', 18],
-      FUSDT: ['0xb7f24e6e708eabfaa9e64b40ee21a5adbffb51d6', 6],
-      WETH: ['0x14f0C98e6763a5E13be5CE014d36c2b69cD94a1e', 18],
-      TOMB: ['0x2317610e609674e53D9039aaB85D8cAd8485A7c5', 0],
-      MIM: ['0x39523112753956d19A3d6a30E758bd9FF7a8F3C0', 9],
-      USDC: ['0x04068da6c83afcfa0e13ba15a6696662335d5b75', 6],
-      FANG: ['0x49894fcc07233957c35462cfc3418ef0cc26129f', 18],
-      'USDT-FTM-LP': ['0xE7e3461C2C03c18301F66Abc9dA1F385f45047bA', 18],
-      '2OMB-FTM-LP': ['0x13Fe199F19c8F719652985488F150762A5E9c3A8', 18],
-      '2SHARE-FTM-LP': ['0x20bc90bB41228cb9ab412036F80CE4Ef0cAf1BD5', 18],
-      '2OMB-2SHARE-LP': ['0xd9B5f00d183df52D717046521152303129F088DD', 18],
-      'MVDOLLAR-USDC-LP': ['0x2b4C76d0dc16BE1C31D4C1DC53bF9B45987Fc75c', 18],
-      MG: ['0x323fA706Ed9925b56F2e5f3487e6D3069ABa4E52', 18],
-    },
-    baseLaunchDate: new Date('2021-06-02 13:00:00Z'),
-    bondLaunchesAt: new Date('2020-12-03T15:00:00Z'),
-    masonryLaunchesAt: new Date('2020-12-11T00:00:00Z'),
-    refreshInterval: 10000,
-  },
+
   production: {
     chainId: ChainId.MAINNET,
     networkName: 'Fantom Opera Mainnet',
